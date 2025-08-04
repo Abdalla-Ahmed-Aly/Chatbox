f0692ae90c4788b6b458a2ccf973446f2a77f1d4
Abdelrahman Ghareeb <you@example.com>
2025-08-04 21:51:02 +0300

    adding apptheme

---
+++ lib/utils/theme/apptheme.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static Color primary = Color(0xffFFFFFF);
+  static Color red = Color(0xffF04A4C);
+  static Color black = Color(0xff1E1E1E);
+  static Color gray = Color(0xff797C7B);
+  static Color lightgreen = Color(0xff20A090);
+  static Color whitegreen = Color(0xffF2F7FB);
+
+  static ThemeData darktheme = ThemeData(
+    scaffoldBackgroundColor: primary,
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: primary,
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: lightgreen,
+      unselectedItemColor: gray,
+    ),
+    // inputDecorationTheme: InputDecorationTheme(
+    //   enabledBorder: OutlineInputBorder(
+    //     borderRadius: BorderRadius.circular(12),
+    //     borderSide: BorderSide(color: AppTheme.black),
+    //   ),
+    //   focusedBorder: OutlineInputBorder(
+    //     borderRadius: BorderRadius.circular(15),
+    //     borderSide: BorderSide(color: AppTheme.primary),
+    //   ),
+    //   errorBorder: OutlineInputBorder(
+    //       borderSide: BorderSide(
+    //         width: 1,
+    //         color: red,
+    //       ),
+    //       borderRadius: BorderRadius.circular(16)),
+    //   focusedErrorBorder: OutlineInputBorder(
+    //       borderSide: BorderSide(
+    //         width: 1,
+    //         color: red,
+    //       ),
+    //       borderRadius: BorderRadius.circular(16)),
+    // ),
+    textTheme: TextTheme(
+      displayLarge: TextStyle(
+        color: primary,
+        fontSize: 68,
+        fontFamily: 'myFonts',
+        fontWeight: FontWeight.w700, //bold font
+      ),
+      displayMedium: TextStyle(
+        color: black,
+        fontSize: 68,
+        fontFamily: 'myFonts',
+        fontWeight: FontWeight.w500, // medium font
+      ),
+      displaySmall: TextStyle(
+        color: primary,
+        fontSize: 40,
+        fontFamily: 'myFonts',
+        fontWeight: FontWeight.w700, // bold font
+      ),
+      headlineLarge: TextStyle(
+        color: primary,
+        fontSize: 40,
+        fontFamily: 'myFonts',
+        fontWeight: FontWeight.w500, // medium font
+      ),
+      headlineMedium: TextStyle(
+        color: primary,
+        fontSize: 20,
+        fontFamily: 'myFonts',
+        fontWeight: FontWeight.w500, // medium font
+      ),
+      headlineSmall: TextStyle(
+        color: primary,
+        fontSize: 18,
+        fontFamily: 'myFonts',
+        fontWeight: FontWeight.w700, // bold font
+      ),
+      titleLarge: TextStyle(
+        color: primary,
+        fontSize: 16,
+        fontFamily: 'myFonts',
+        fontWeight: FontWeight.w700, // bold font
+      ),
+      titleMedium: TextStyle(
+        color: primary,
+        fontSize: 16,
+        fontFamily: 'myFonts',
+        fontWeight: FontWeight.w500, // medium font
+      ),
+      titleSmall: TextStyle(
+        color: primary,
+        fontSize: 16,
+        fontFamily: 'myFonst',
+        fontWeight: FontWeight.w400, // regular font
+      ),
+      bodyLarge: TextStyle(
+        color: primary,
+        fontSize: 14,
+        fontFamily: 'myFonts',
+        fontWeight: FontWeight.w300, // light font
+      ),
+      bodyMedium: TextStyle(
+        color: primary,
+        fontSize: 14,
+        fontWeight: FontWeight.w500, // medium font
+      ),
+      bodySmall: TextStyle(
+        color: primary,
+        fontSize: 12,
+        fontFamily: 'myFonts',
+        fontWeight: FontWeight.w300, // light font
+      ),
+    ),
+  );
+}
