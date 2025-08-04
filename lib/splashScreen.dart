39491612c6a0aafb92b1ad375e826e57682107b0
Abdelrahman Ghareeb <you@example.com>
2025-08-04 21:51:02 +0300

    Edit main File

---
+++ lib/splashScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Splashscreen extends StatefulWidget {
+  static const String routeName = '/splashScreen';
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
